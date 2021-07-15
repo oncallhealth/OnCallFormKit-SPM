54a502368cdf65e187cd6f0b189992ae28720103
Domenic Bianchi <domenic@oncallhealth.com>
2021-07-15 14:10:49 -0400

    Save

---
+++ OnCallFormKit.xcframework/ios-arm64_x86_64-simulator/OnCallFormKit.framework/Headers/OnCallFormKit.h
@@ -0,0 +1,18 @@
+//
+//  OnCallFormKit.h
+//  OnCallFormKit
+//
+//  Created by Domenic Bianchi on 2021-07-07.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OnCallFormKit.
+FOUNDATION_EXPORT double OnCallFormKitVersionNumber;
+
+//! Project version string for OnCallFormKit.
+FOUNDATION_EXPORT const unsigned char OnCallFormKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OnCallFormKit/PublicHeader.h>
+
+
