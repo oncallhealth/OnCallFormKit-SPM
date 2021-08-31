940b80602d54bea93a4223233ec3cf191abd3b8c
DomenicBianchi01 <bianchidomenic@gmail.com>
2021-08-31 11:24:21 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "OnCallFormKit",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "OnCallFormKit",
